b44f03e7db37114247a9523adaddbb76e346397f
MUHAMMET FURKAN USLU <u_furkan96@hotmail.com>
2022-02-20 17:09:33 +0300

    20022217:09

--- src/app/services/brand.service.ts
+++ src/app/services/brand.service.ts
@@ -11,7 +11,7 @@ export class BrandService {
   apiUrl = 'https://localhost:44389/api/';
   constructor(private httpClient: HttpClient) {}
   getBrands(): Observable<ListResponseModel<Brand>> {
-    let newPath= this.apiUrl +"brands/getall +5"
+    let newPath= this.apiUrl +"brands/getall"
     return this.httpClient
     .get<ListResponseModel<Brand>>(newPath);
   }
